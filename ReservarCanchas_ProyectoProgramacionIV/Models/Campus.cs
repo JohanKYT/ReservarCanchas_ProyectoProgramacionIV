cf21484eb3fdd8a365fb355de2f1c8fb6a0e3032
JohanKYT <kevin.maquis@udla.edu.ec>
2025-04-25 23:54:34 -0500

    Primer commit, inicio de proyecto y creacion de modelos bases para usar en el proyecto

--- ReservarCanchas_ProyectoProgramacionIV/Models/Campus.cs
+++ ReservarCanchas_ProyectoProgramacionIV/Models/Campus.cs
@@ -1,16 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ReservarCanchas_ProyectoProgramacionIV.Models
 {
-    public class Facultad
+    public class Campus
     {
         [Key]
         public int Id { get; set; }
         [MaxLength(100)]
         public string Nombre { get; set; }
-        public int CampusId { get; set; }
-        [ForeignKey("CampusId")]
-        public Campus? Campus { get; set; }
+        [MaxLength(100)]
+        public string Direccion { get; set; }
     }
 }
